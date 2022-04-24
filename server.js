4b7dca17bf16d16917791cf675bc78dc4f46dacd
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-04-24 18:05:28 +0200

    28.4

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+const tasks = [];
+
+const server = app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running...');
+});
+
+app.use((req, res) => {
+    res.status(404).send({ message: 'Not found...' });
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    socket.emit('updateData', tasks);
+
+    socket.on('addTask', (taskData) => {
+        tasks.push(taskData);
+        socket.broadcast.emit('addTask', taskData);
+    });
+
+    socket.on('removeTask', (taskId) => {
+        const taskIdIndex = tasks.findIndex(task => task.id === taskId);
+        tasks.splice(taskIdIndex, 1);
+        socket.broadcast.emit('removeTask', taskId);
+    });
+});
